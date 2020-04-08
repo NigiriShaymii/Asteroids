adb92552feb197f4054dd4fb0c98e6a037477ddf
Johnny Lee <johnny.lee0806@gmail.com>
2020-04-08 16:58:47 -0700

    Asteroid

---
+++ laser.js
@@ -0,0 +1,17 @@
+
+function Laser(spos, angle) {
+  this.pos = createVector(spos.x, spox.y);
+  this.vel = p5.Vector.fromAngle(angle);
+
+  this.update = function() {
+    this.pos.add(this.vel);
+  }
+
+  this.render = function() {
+    push();
+    stroke(255);
+    strokeWeight(4);
+    point(this.pos.x, this.pos.y);
+    pop();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,49 @@
+var ship;
+var asteroids = [];
+var lasers = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
+  for(var i = 0; i < 5; i++)
+  {
+    asteroids.push(new Asteroid());
+  }
+  asteroids.push(new Asteroid());
+}
+
+function draw() {
+  background(0);
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+
+  for( var i = 0; i < asteroids.length; i++){
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
+
+  for( var i = 0; i < lasers.length; i++){
+    lasers[i].render();
+    lasers[i].update();
+  }
+}
+
+function keyReleased(){
+  ship.setRotation(0);
+  ship.boosting(false);
+}
+
+function keyPressed
+if(key == ' ') {
+  lasers.push(new Laser(ship.pos, ship.heading));
+  } else if(keyCode == RIGHT_ARROW){
+    ship.setRotation(0.1);
+  } else if(keyCode == LEFT_ARROW){
+    ship.setRotation(-0.1);
+  } else if(keyCode == UP_ARROW){
+    ship.boosting(true);
+  }
+}
