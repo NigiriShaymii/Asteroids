4d41af8da88d0a204b725faf4b0f2ce3856b1e2d
Johnny Lee <johnny.lee0806@gmail.com>
2020-04-15 14:40:53 -0700

    Winlose

--- sketch.js
+++ sketch.js
@@ -1,22 +1,55 @@
 var ship;
 var asteroids = [];
 var lasers = [];
+let space = "zoom";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER);
+  textSize(40);
   ship = new Ship();
   for (var i = 0; i < 5; i++) {
     asteroids.push(new Asteroid());
   }
   asteroids.push(new Asteroid());
+
 }
 
 function draw() {
+
+  switch (space) {
+    case ('zoom'):
+      zoomScreen();
+      break;
+    case ('gamePlay'):
+      gamePlay();
+      break;
+    case ('youWon'):
+
+      break;
+    case ('youLose'):
+      youLose();
+      break;
+    default:
+      break;
+  }
+
+}
+
+function zoomScreen() {
+  background(0);
+  fill(255);
+  text('Press the "S" key to start playing...', windowWidth / 2, windowHeight / 2);
+}
+
+
+function gamePlay() {
   background(0);
 
   for (var i = 0; i < asteroids.length; i++) {
     if (ship.hits(asteroids[i])) {
       console.log('ouch!');
+      space = 'youLose';
     }
     asteroids[i].render();
     asteroids[i].update();
@@ -29,28 +62,35 @@ function draw() {
     if (lasers[i].offscreen()) {
       lasers.splice(i, 1);
     } else {
-    for (var j = asteroids.length - 1; j >= 0; j--) {
-      if (lasers[i].hits(asteroids[j])) {
-        if (asteroids[j].r > 10) {
-          var newAsteroids = asteroids[j].breakup();
-          asteroids = asteroids.concat(newAsteroids);
+      for (var j = asteroids.length - 1; j >= 0; j--) {
+        if (lasers[i].hits(asteroids[j])) {
+          if (asteroids[j].r > 10) {
+            var newAsteroids = asteroids[j].breakup();
+            asteroids = asteroids.concat(newAsteroids);
+          }
+          asteroids.splice(j, 1);
+          lasers.splice(i, 1);
+          break;
         }
-        asteroids.splice(j, 1);
-        lasers.splice(i, 1);
-        break;
       }
     }
   }
+
+  console.log(lasers.length);
+
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
 }
 
-
-console.log(lasers.length);
-
-ship.render();
-ship.turn();
-ship.update();
-ship.edges();
+function youLose() {
+  background(0);
+  fill(255);
+  text('You lose.', windowWidth / 2, windowHeight / 2);
+  text('\n\n Refresh the webpage to play again', windowWidth / 2, windowHeight / 2);
 }
+
 
 function keyReleased() {
   ship.setRotation(0);
@@ -58,6 +98,11 @@ function keyReleased() {
 }
 
 function keyPressed() {
+
+  if (key === 's') {
+    space = 'gamePlay';
+  }
+
   if (key == ' ') {
     lasers.push(new Laser(ship.pos, ship.heading));
   } else if (keyCode == RIGHT_ARROW) {
